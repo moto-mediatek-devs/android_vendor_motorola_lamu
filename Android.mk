8441ebf1946b9a8561de3a2d0005789acd41d2c6
R0rt1z2 <roger@r0rt1z2.com>
2026-05-25 17:04:14 +0200

    lamu: Initial vendor tree
    Change-Id: I4e29fd43351136a88e5face71331ba830ab58089

---
+++ Android.mk
@@ -0,0 +1,19 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),lamu)
+
+$(call add-radio-file-sha1-checked,radio/dtbo.img,746ea9905bbe4296dfeee0d31f5f43e350c521ff)
+$(call add-radio-file-sha1-checked,radio/gz.img,02b5d492b99894b7b6c9c0d2e753b2d03d96f11c)
+$(call add-radio-file-sha1-checked,radio/lk.img,391189af5515ab3340111ce7d0c4cf2567de1dcc)
+$(call add-radio-file-sha1-checked,radio/modem.img,616d5c60c76479c487748bba896a72fac0525185)
+$(call add-radio-file-sha1-checked,radio/preloader_raw.img,1d099339f9e37c1116066c3f946f6c7d47a257f9)
+$(call add-radio-file-sha1-checked,radio/scp.img,003b7c593348d9a875d2a1fa281d5aedabdc8c07)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,9a16ad1f691a83adcf62978e36308bcf4ab672d9)
+$(call add-radio-file-sha1-checked,radio/sspm.img,7e39fe9796471b521bd73f674635b4e86e79c75f)
+$(call add-radio-file-sha1-checked,radio/tee.img,559975088dad316ff7539eab7aeb538939571bc4)
+
+endif
---
+++ BoardConfigVendor.mk
@@ -0,0 +1,14 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+AB_OTA_PARTITIONS += \
+    dtbo \
+    gz \
+    lk \
+    modem \
+    preloader_raw \
+    scp \
+    spmfw \
+    sspm \
+    tee
